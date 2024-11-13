0bc81277a5a3f5e09bfa01b94f3554250cb9856f
Rafael <rosselgalarzarafael@gmail.com>
2024-11-12 23:52:12 -0500

    descomentando todos los controllers

--- app.py
+++ app.py
@@ -11,34 +11,34 @@ from src.controllers.costo_unitario_controller import  mostrar_costo_unitario_pr
 from src.controllers.inventario_final_controller import mostrar_inventario_final
 
 if __name__ == "__main__":
-    # print("Calculando Presupuesto de Ventas...")
-    # mostrar_presupuesto_ventas()
+    print("Calculando Presupuesto de Ventas...")
+    mostrar_presupuesto_ventas()
 
-    # print("\nCalculando Saldo de Clientes y Flujo de Entradas...")
-    # mostrar_saldo_clientes_flujo_entradas()
+    print("\nCalculando Saldo de Clientes y Flujo de Entradas...")
+    mostrar_saldo_clientes_flujo_entradas()
 
-    # print("\nCalculando Presupuesto de Producción...")
-    # mostrar_presupuesto_produccion()
+    print("\nCalculando Presupuesto de Producción...")
+    mostrar_presupuesto_produccion()
 
-    # print("\nCalculando Presupuesto de Requerimiento de Materiales...")
-    # mostrar_presupuesto_materiales()
+    print("\nCalculando Presupuesto de Requerimiento de Materiales...")
+    mostrar_presupuesto_materiales()
 
-    # print("\nCalculando Presupuesto de Compra de Materiales...")
-    # mostrar_presupuesto_compras()
+    print("\nCalculando Presupuesto de Compra de Materiales...")
+    mostrar_presupuesto_compras()
     
-    # print("\nDeterminación del Saldo de Proveedores y Flujo de Salidas...")
-    # mostrar_saldo_proveedores()
+    print("\nDeterminación del Saldo de Proveedores y Flujo de Salidas...")
+    mostrar_saldo_proveedores()
     
-    # print("\nCalculando Presupuesto de Mano de Obra Directa...")
-    # mostrar_presupuesto_mano_obra()
+    print("\nCalculando Presupuesto de Mano de Obra Directa...")
+    mostrar_presupuesto_mano_obra()
     
-    #print("Calculando Presupuesto de Gastos Indirectos de Fabricación...")
-    #mostrar_presupuesto_gastos_indirectos()
+    print("Calculando Presupuesto de Gastos Indirectos de Fabricación...")
+    mostrar_presupuesto_gastos_indirectos()
     
     print("Calculando Presupuesto de Gastos de Operación...")
-    #mostrar_presupuesto_gastos_operacion()
+    mostrar_presupuesto_gastos_operacion()
     
     print("Calculando Determinación del Costo Unitario de Productos Terminados...")
-    #mostrar_costo_unitario_producto()
+    mostrar_costo_unitario_producto()
     print("Calculando Valuación de Inventarios Finales...")
     mostrar_inventario_final()
