5d539f7a44bfecb344e7e7a751f3aa0697af66a6
Rafael <rosselgalarzarafael@gmail.com>
2024-11-12 22:44:58 -0500

    tabulador para clientes

--- src/services/ventas_service.py
+++ src/services/ventas_service.py
@@ -1,14 +1,20 @@
-def calcular_importe_venta(unidades, precio):
-    return unidades * precio
+presupuesto_cache = None  # Variable para almacenar el resultado del cálculo
 
-def calcular_presupuesto_ventas(productos):
+def calcular_presupuesto_ventas(productos=None):
+    global presupuesto_cache
+    if presupuesto_cache is not None and productos is None:
+        return presupuesto_cache  # Usar el resultado almacenado si ya existe
+
+    if productos is None:
+        raise ValueError("Debe proporcionar datos de productos para calcular el presupuesto de ventas")
+
     total_por_semestre = [0, 0]
     resultado = {}
 
     for producto, datos in productos.items():
         importes = []
         for i in range(2):
-            importe = calcular_importe_venta(datos["unidades"][i], datos["precio"][i])
+            importe = datos["unidades"][i] * datos["precio"][i]
             importes.append(importe)
             total_por_semestre[i] += importe
         
@@ -18,4 +24,5 @@ def calcular_presupuesto_ventas(productos):
     resultado["Total Semestre"] = total_por_semestre
     resultado["Total Anual"] = sum(total_por_semestre)
 
+    presupuesto_cache = resultado  # Almacenar el resultado en cache
     return resultado
